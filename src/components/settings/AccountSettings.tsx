33daa3653ebcc44e724e23e4f28fa214e62ab120
Marko Moev <marko.moev.business@gmail.com>
2025-09-04 23:55:57 +0300

    Account settings design update --- in progress

--- src/components/settings/AccountSettings.tsx
+++ src/components/settings/AccountSettings.tsx
@@ -2,23 +2,37 @@
 
 export default function accSettings(){
     return(
-        <div className ='w-auto bg-black/20 pl-20 pr-20 rounded-2xl'>
-            <div className = 'w-full flex flex-row gap-20'>
-                <div>
-                {/* Profile picture container */}
-                <img 
-                    src="" 
-                    alt="Profile picture" 
-                    className = 'md:w-35 md:h-35 bg-black'
-                />
+        <div className ='w-auto bg-white/20 pl-10 pr-20 rounded-2xl border border-black/10 shadow-lg'>
+            <div className = 'w-full flex flex-col gap-5'>
+                {/* Username */}
+                <div className ='mb-3'>
+                    <div className = 'mt-3'>
+                        <p
+                            className ='font-semibold text-lg bg-gradient-to-r from-purple-500 to-blue-500 bg-clip-text text-transparent'>
+                            Profile info   
+                        </p>
+                    </div>
+                </div> 
 
+                <div className ='w-1/2 flex gap-5'>
+                    <input readOnly
+                        className = 'bg-black/18 text-white p-1 pl-2 pr-2 rounded-md border border-black/10 shadow-sm'
+                        type ='email'
+                        placeholder ='Email here'
+                    />
+
+                    <input readOnly
+                        className = 'bg-black/18 text-white p-1 pl-2 pr-2 rounded-md border border-black/10 shadow-sm'
+                        type="text"
+                        placeholder ='Username here'/>
                 </div>
 
-                <div className = 'mt-3'>
-                    <p
-                        className ='font-semibold text-lg'>
-                        Username    
-                    </p>
+                <div>
+                    <button className ='pt-1 pb-1 pl-3 pr-3 cursor-pointer border border-black/10 bg-gradient-to-r from-purple-500 to-blue-500 bg-clip-text text-transparent rounded-2xl font-bold text-md shadow-lg'>
+                        Edit
+                    </button>
+                    <button>Save</button>
+                    <button>delete profile</button>
                 </div>
             </div>
         </div>
--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -5,7 +5,9 @@ import AccountSettings from '../components/settings/AccountSettings'
 export default function SettingsPage(){
     return(
         <div className = 'w-full flex flex-col'>
-            <TopBar/>
+            <div className = 'w-full flex items-center justify-center'>
+                <TopBar/>
+            </div>
             <div>
                 <div className = 'w-full h-auto flex flex-row items-center justify-center mt-20 gap-15'>
                     <SettingsMenu/>
