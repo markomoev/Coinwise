53e2140f9ab07517d6875066140b04af53d198ef
Marko Moev <marko.moev.business@gmail.com>
2025-09-24 09:42:52 +0300

    connected to db, adding name works

--- src/components/transactions/TransactionPopup.tsx
+++ src/components/transactions/TransactionPopup.tsx
@@ -1,8 +1,36 @@
+import {supabase} from '../../client'
+
+import { useState } from 'react';
+
 type Props = {
     closePopup: () => void;
 }
 
 export default function TransactionPopup({closePopup}: Props) {
+    // storing all the clinets info from the popups
+    const [name, setName] = useState('');
+
+
+    const addTransaction = async () => {
+        // add transaction logic
+        const { data: { user } } = await supabase.auth.getUser()
+        const currentUser = user?.id;
+
+
+        const { error } = await supabase
+        .from('Transactions')
+        .insert([{ 
+            user_id: currentUser,
+            name: name,
+        }])
+
+        if(error){
+            alert('Error in adding transaction!');
+            console.error(error.message);
+        }
+        
+        closePopup();
+    }
     
     return(
         <div className = 'bg-white border border-black/10 w-full h-auto rounded-2xl flex flex-col shadow-md pb-3'>
@@ -20,7 +48,8 @@ export default function TransactionPopup({closePopup}: Props) {
                         type="text" 
                         placeholder = 'Name' 
                         className="bg-gray-600/15 text-black/70 pl-2 rounded-md"
-
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
                         />
                 </div>
             
@@ -74,15 +103,18 @@ export default function TransactionPopup({closePopup}: Props) {
                 <textarea 
                     name="note" 
                     id="note"
+                    placeholder='Add a note...'
                     className = 'w-full bg-gray-600/15 pl-2 rounded-md pt-1'>
-                    Note
                 </textarea>
             </div>
 
             <div className = 'mt-10 w-full flex flex-row justify-between pl-5 pr-5'>
-                <button className = 'text-lg font-bold flex items-center justify-center border-black/10 border hover:shadow-lg rounded-xl pb-1 pr-3 pl-3 pt-1 cursor-pointer bg-gradient-to-r from-purple-500 to-blue-500 bg-clip-text text-transparent'>
+                <button
+                    onClick={addTransaction} 
+                    className = 'text-lg font-bold flex items-center justify-center border-black/10 border hover:shadow-lg rounded-xl pb-1 pr-3 pl-3 pt-1 cursor-pointer bg-gradient-to-r from-purple-500 to-blue-500 bg-clip-text text-transparent'>
                     Add
                 </button>
+
                 <button 
                     onClick={closePopup}
                     className = 'text-lg font-bold flex items-center justify-center border-black/10 border hover:shadow-lg rounded-xl pb-1 pr-3 pl-3 pt-1 cursor-pointer bg-gradient-to-r from-purple-500 to-blue-500 bg-clip-text text-transparent'>
